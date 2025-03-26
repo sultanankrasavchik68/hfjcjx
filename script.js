3447dc4307080795951a62cf5e1119367ff60def
sultanankrasavchik68 <sultananvarov34@gmail.com>
2025-03-26 12:58:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// let name = 'Abdulloh';
+// let num = 435;
+
+// let user = {
+//     name: 'John',
+//     age: 30,
+//     city: 'New York'
+// };
+
+
+// console.log(name)
+
+// console.log(user)
+// console.log(user.city)
+// console.log(user['age'])
+
+// delete user.age;
+// console.log(user)
+
+
+let students = {}
+
+students.name = prompt('Укажите имя');
+
+students.id = prompt('Укажите id');
+
+students.password = prompt('Укажите password');
+
+
+console.log(students)
